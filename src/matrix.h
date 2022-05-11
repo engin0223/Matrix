223cf40d22bca7b224265b008acc91cfaa5eb20c
engin0223 <99678175+engin0223@users.noreply.github.com>
2022-05-11 13:37:26 +0300

    Update matrix.h
    Added documentation, LU decomposition, and macro for getting the name of a matrix.

--- src/matrix.h
+++ src/matrix.h
@@ -10,6 +10,11 @@
 #include <sstream> 
 #include <string> 
 
+
+// Class for matrix.
+// To create a matrix choose the type of entries->T, create a vector of vector(s)->myVec
+// then write matrix<T> matrix_name(myVec).
+//(E.g "matrix<int> A({{1,2,3}})" creates a 1x3 matrix(i.e row vector) which is [1,2,3]))
 template <typename T>
 class matrix {
 public:
@@ -21,7 +26,7 @@ public:
 		n = int(B[0].size());
 
 		X = B;
-	};
+	}
 
 	T get(int i, int j) {
 		return X[i - 1][j - 1];
@@ -34,16 +39,43 @@ public:
 };
 
 
+template <class C>
+#define name(C) #C  // gets the variable name of class C object.
+
+
+void s() {}
+
+
+
+
+// Creates a ixj matrix whose entries are "type(T) varible_name(n)".(E.g. "double number").
 template <typename T>
 matrix<T> matrix_n(int i, int j, T n) {
 	std::vector<std::vector<T>> B;
-	std::vector<int> row(j, n);
+	std::vector<T> row(j, n);
 	for (int k = 0; k < i; ++k) {
 		B.push_back(row);
 	}
 	return matrix<T>(B);
 }
 
+
+// Creates a NxN matrix Identity matrix.(i.e matrix[i][i] = 1 for all i that is 1<=i<=N and all other entries are 0)
+template <typename T>
+matrix<T> I(int N) {
+	std::vector<std::vector<T>> B;
+	std::vector<T> row(N, 0);
+	for (int k = 0; k < N; ++k) {
+		B.push_back(row);
+	}
+	for (int i = 0; i < N; ++i) {
+		B[i][i] = 1;
+	}
+	return matrix<T>(B);
+}
+
+
+// return determinant of matrix.
 template <typename T>
 T det(matrix<T> B) {
 	T sum{};
@@ -54,12 +86,13 @@ T det(matrix<T> B) {
 		return B.get(1, 1);
 	}
 	for (int j = 1; j <= B.m; ++j) {
-		sum += (-2 * ((1 + j) % 2) + 1) * B.get(1, j) * det(get_minor(1, j, B));
+		sum += (-2 * ((1 + j) % 2) + 1) * B.get(1, j) * get_minor(1, j, B);
 	}
 	return sum;
 }
 
 
+// returns adjugate of matrix.
 template <typename T>
 matrix<T> adj(matrix<T> B) {
 	matrix<T> G = B;
@@ -77,19 +110,32 @@ matrix<T> adj(matrix<T> B) {
 	else {
 		for (int i = 1; i <= B.m; ++i) {
 			for (int j = 1; j <= B.n; ++j) {
-				G.rep(i, j, (-2 * ((i + j) % 2) + 1) * det(get_minor(j, i, B)));
+				G.rep(i, j, (-2 * ((i + j) % 2) + 1) * get_minor(j, i, B));
 			}
 		}
 		return G;
 	}
 }
 
+
+// returns invert of matrix.
 template <typename T>
 matrix<T> inv(matrix<T> B) {
-	return adj(B) * (pow(det(B), -1.0)) * 1.0;
+	try {
+		if (det(B) == 0) {
+			throw (0);
+		}
+		else {
+			return adj(B) * (pow(det(B), -1.0)) * 1.0;
+		}
+	}
+	catch (int n) {
+		std::cout << "determinant of the matrix is zero therefore the matrix is singular(not-invertible)";
+	}
 }
 
 
+// returns transpose of matrix.
 template <typename K>
 matrix<K> T(matrix<K> B) {
 	matrix<K> G = B;
@@ -101,6 +147,8 @@ matrix<K> T(matrix<K> B) {
 	return G;
 }
 
+
+// returns trace of matrix.
 template <typename T>
 T Tr(matrix<T> B) {
 	T sum{};
@@ -111,8 +159,34 @@ T Tr(matrix<T> B) {
 }
 
 
+// Takes LU decomposition of matrix and returns a vector with entries are L and U.(i.e {L, U})
 template <typename T>
-void show(matrix<T> B, int precision = 5) {
+std::vector<matrix<T>> LU(matrix<T> B) {
+	int N = B.m;
+	matrix<T> A_n(B.X);
+	matrix<T> L(I<double>(N).X);
+	matrix<T> L_n(I<double>(N).X);
+	for (int n = 1; n <= N - 1; ++n) {
+		L_n = I<double>(N);
+		for (int i = n + 1; i <= N; ++i) {
+			L_n.rep(i, n, -(A_n.get(i, n)) / (A_n.get(n, n)));
+		}
+		for (int i = n + 1; i <= N; ++i) {
+			for (int j = 1; j <= N; ++j) {
+				A_n.rep(i, j, A_n.get(i, j) + A_n.get(n, j) * L_n.get(i, n));
+			}
+		}
+		L = L * inv(L_n);
+	}
+	matrix<T> U((inv(L) * B).X);
+	return { L, U };
+
+}
+
+
+// prints a matrix's entries with a precision(default is 5).
+template <typename T>
+void show(matrix<T> B, int precision=5) {
 	for (int i = 1; i <= B.m; ++i) {
 		for (int j = 1; j <= B.n; ++j) {
 			if (precision != 0) {
@@ -127,6 +201,7 @@ void show(matrix<T> B, int precision = 5) {
 	}
 }
 
+// multiplies the matrix with itself n times and returns that matrix.
 template <typename T>
 matrix<T> mul_n(matrix<T> B, int n) {
 	matrix<T> G = B;
@@ -137,8 +212,9 @@ matrix<T> mul_n(matrix<T> B, int n) {
 }
 
 
+// returns the (m,n) minor of the matrix.
 template <typename T>
-matrix<T> get_minor(int m, int n, matrix<T> B) {
+T get_minor(int m, int n, matrix<T> B) {
 	std::vector<std::vector<T>> G = B.X;
 	int k = 0;
 	for (int i = 1; i <= B.m; ++i) {
@@ -151,11 +227,13 @@ matrix<T> get_minor(int m, int n, matrix<T> B) {
 		}
 	}
 	matrix<T> C(G);
-	return C;
+	T M = det(C);
+	return M;
 }
 
 
 
+// returns the product of matrices. (be aware that order matters (i.e A*B!=B*A))
 template <typename T>
 matrix<T> operator*(matrix<T> a, matrix<T> b) {
 	if (a.n == b.m) {
@@ -174,6 +252,7 @@ matrix<T> operator*(matrix<T> a, matrix<T> b) {
 	}
 }
 
+// returns the sum of matrices.
 template <typename T>
 matrix<T> operator+(matrix<T> a, matrix<T> b) {
 	if (a.m == b.m && a.n == b.n) {
@@ -187,6 +266,7 @@ matrix<T> operator+(matrix<T> a, matrix<T> b) {
 	}
 }
 
+// returns the difference of matrices.
 template <typename T>
 matrix<T> operator-(matrix<T> a, matrix<T> b) {
 	if (a.m == b.m && a.n == b.n) {
@@ -203,7 +283,7 @@ matrix<T> operator-(matrix<T> a, matrix<T> b) {
 
 template <typename T>
 matrix<double> operator*(double a, matrix<T> b) {
-	for (int i = 1; i <= b.m; ++i) {
+ 	for (int i = 1; i <= b.m; ++i) {
 		for (int j = 1; j <= b.n; ++j) {
 			b.rep(i, j, a * b.get(i, j) * 1.0);
 		}
@@ -211,6 +291,8 @@ matrix<double> operator*(double a, matrix<T> b) {
 	return b;
 }
 
+// above and below operators are the same. Reason for 2 seemingly same operator is order matters for operators so you can't do k*A and A*k at the same time.
+// returns the scalar multiplication of matrix. 
 
 template <typename T>
 matrix<double> operator*(matrix<T> b, double a) {
